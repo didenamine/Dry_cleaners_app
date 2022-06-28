bc18337140307a9177c336ce22e5656975f5b7be
DidenAmine <D19amine@gmail.com>
2022-06-28 14:03:42 +0100

    Updated

--- setup.py
+++ setup.py
@@ -12,8 +12,9 @@ target=Executable(
 setup(
     name="Pressing",
     version="1.0",
-    description="Crreator : Diden Amine",
-    author="Diden amine",
+    description="Dry_cleaners_app is a desktop software made to help the owners to get rid of the paper use",
+    author="Diden Amine",
+    author_email="d19amine@gmail.com",
     options={'build_exe' :{'include_files':files}},
     executables=[target]
 )
